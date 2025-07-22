c496f7e8ba562eb0da18b529cb6a206f07b2e624
crytek1012 <lucianpetru222@gmail.com>
2025-07-22 22:18:05 +0300

    added cachedAt property on cache and option for force fetching if data is older than the provided value

--- src/structures/banshare-manager.ts
+++ src/structures/banshare-manager.ts
@@ -1,23 +1,41 @@
 import { IRawBanshare } from "@/types/database";
-import { CollectionManager, defaultFetchOptions } from "./collection-manager";
+import { CollectionManager, defaultFetchOptions, FetchOptions } from "./collection-manager";
 
-export default class BanshareManager extends CollectionManager<IRawBanshare> {
+export interface IBanshare extends IRawBanshare {
+    cachedAt: number;
+}
+
+export interface IBanshareFetchOptions extends FetchOptions {
+    /**
+     * Force fetch if the cache is older than the provided value
+     */
+    fetchIfOlderThan: number;
+}
+
+export const banshareFetchOptions: IBanshareFetchOptions = {
+    ...defaultFetchOptions,
+    fetchIfOlderThan: 0
+}
+
+export default class BanshareManager extends CollectionManager<IRawBanshare, IBanshare> {
 
     /**
      * Fetch a banshare
      * @param id 
      * @returns 
      */
-    async fetch(id: string, options = defaultFetchOptions) {
-        const opt = { ...defaultFetchOptions, ...options };
+    async fetch(id: string, options = banshareFetchOptions) {
+        const opt = { ...banshareFetchOptions, ...options };
 
         const cached = this.cache.get(id);
-        if (cached && opt.checkCache) return cached;
+        if (cached && opt.checkCache && (cached.cachedAt + opt.fetchIfOlderThan) > Date.now()) return cached;
 
         const rawData = await this.collection.findOne({ id });
         if (!rawData) return null;
 
-        this.cache.set(rawData.id, rawData)
+        const cache: IBanshare = { ...rawData, cachedAt: Date.now() };
+
+        this.cache.set(rawData.id, cache)
         return rawData;
     }
 
